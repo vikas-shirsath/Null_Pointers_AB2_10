84ba3622cf7a33cedc466711a413e301e94f56c9
nandkishor vasi <nandkishorvasi@gmail.com>
2025-03-01 22:55:51 +0530

    update 03

--- backend/demoPii/src/main/java/com/nvasi/demoPii/controller/PiiController.java
+++ backend/demoPii/src/main/java/com/nvasi/demoPii/controller/PiiController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins="http://localhost:5173")
 @RestController
 @RequestMapping("/api")
 public class PiiController {
